18a010a9d9214973f2d141b0c57ed08c89719648
Jennifer-Maia <Jennifer-Maia>
2025-08-21 21:42:37 +0000

    salvando como python ao inves de ipynb

---
+++ src/ingestao.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+import requests
+import json
+import datetime
+
+url = 'https://pokeapi.co/api/v2/pokemon?limit=1500'
+
+resp = requests.get(url)
+data = resp.json()
+
+
+data_save = data['results']
+data_save
+
+now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+path = f"/Volumes/raw/pokemon/pokemon_raw/pokemons_list/{now}.json"
+with open(path, 'w') as open_file:
+  json.dump(data_save, open_file)
+
+
+# COMMAND ----------
+
+dbutils.fs.mkdirs("/Volumes/raw/pokemon/pokemon_raw/pokemons_list")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/Volumes/raw/pokemon/pokemon_raw/pokemons_list")
